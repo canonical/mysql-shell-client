1d109f04bf57c0cebc8b6f19823dadc6e9c51eb8
Sinclert Pérez <sinclert.perez@canonical.com>
2025-12-02 09:49:47 +0100

    Set ExecutionError context to None

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog][docs-changelog], and the version adher
 
 
 ## Unreleased
+### Fixed
+- Possible password leak in LocalExecutor traceback info.
 
 ## [0.1.0][changes-0.1.0] - 2025-12-01
 ### Added
--- src/mysql_shell/executors/errors/runtime.py
+++ src/mysql_shell/executors/errors/runtime.py
@@ -4,3 +4,8 @@
 
 class ExecutionError(RuntimeError):
     """MySQL shell execution error."""
+
+    # The context attribute is overridden so passwords
+    # are not logged in when showing the traceback
+    # of the previously raised exception
+    __context__ = None
