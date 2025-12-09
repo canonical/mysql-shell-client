b9e06139e6ca12ff08417dd52ac54dad741a9300
Sinclert Pérez <sinclert.perez@canonical.com>
2025-12-09 08:50:40 +0100

    Add LocalExecutor password strip

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog][docs-changelog], and the version adher
 
 
 ## Unreleased
+### Fixed
+- Possible password leak in LocalExecutor.
 
 ## [0.3.0][changes-0.3.0] - 2025-12-05
 ### Added
--- src/mysql_shell/executors/errors/runtime.py
+++ src/mysql_shell/executors/errors/runtime.py
@@ -1,11 +1,20 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
+
 
 class ExecutionError(RuntimeError):
     """MySQL shell execution error."""
 
-    # The context attribute is overridden so passwords
-    # are not logged in when showing the traceback
-    # of the previously raised exception
-    __context__ = None
+    def __init__(self, message: str | None = None):
+        """Initialize the error."""
+        if not message:
+            return
+
+        if not message.startswith("MySQL Error"):
+            message = json.loads(message)
+            message = message["error"] if message else message
+            message = message["message"] if message else message
+
+        super().__init__(message)
